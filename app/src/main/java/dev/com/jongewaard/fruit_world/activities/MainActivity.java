f695a898076dcb97b543fe3d3a6ef50300984cfc
german <germanjongewaard@gmail.com>
2017-11-13 09:43:20 +0100

    More code...

--- app/src/main/java/dev/com/jongewaard/fruit_world/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/fruit_world/activities/MainActivity.java
@@ -2,6 +2,8 @@ package dev.com.jongewaard.fruit_world.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -88,6 +90,63 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         else
             Toast.makeText(this, "The best fruit from " + fruit.getOrigin() + " is " + fruit.getName(), Toast.LENGTH_SHORT).show();
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflamos el option menu con nuestro valor
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        //Despues de inflado, recogemos las referencias de los botones que nos interesan.
+        this.itemListView = menu.findItem(R.id.list_view);
+        this.itemGridView = menu.findItem(R.id.list_view);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Eventos para los clicks en los botones del option menu
+        switch (item.getItemId()){
+            case R.id.add_fruit:
+                this.addFruit(new Fruit("Added nº" + (++counter), R.mipmap.ic_new_fruit, "Unknown" ));
+                return true;
+            case R.id.list_view:
+                this.switchListGridView(this.SWITCH_TO_LIST_VIEW);
+                return true;
+            case R.id.grid_view:
+                this.switchListGridView(this.SWITCH_TO_GRID_VIEW);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Método para cambiar entre Grid/List view
+    private void switchListGridView(int option) {
+        // Método para cambiar entre Grid/List view
+        if (option == SWITCH_TO_LIST_VIEW) {
+            // Si queremos cambiar a list view, y el list view está en modo invisible...
+            if(this.listView.getVisibility() == View.INVISIBLE){
+                // ... escondemos el grid view, y enseñamos su botón en el menú de opciones
+                this.gridView.setVisibility(View.INVISIBLE);
+
+            }
+        }
+
+
+
+
+
+
+    }
+
+    private void addFruit(Fruit fruit) {
+        this.fruits.add(fruit);
+        //Avisamos del cambio en ambos adapters
+        this.adapterListView.notifyDataSetChanged();
+        this.adapterGridView.notifyDataSetChanged();
+    }
+
 
     private List<Fruit> getAllFruits(){
         List<Fruit> list = new ArrayList<Fruit>(){{
