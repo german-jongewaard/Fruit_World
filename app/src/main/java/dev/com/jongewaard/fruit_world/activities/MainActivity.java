3eb5c57c5d881e40cdf111bda9a9d8affa80a0c4
german <germanjongewaard@gmail.com>
2017-11-13 09:04:15 +0100

    More code...

--- app/src/main/java/dev/com/jongewaard/fruit_world/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/fruit_world/activities/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dev.com.jongewaard.fruit_world.R;
+import dev.com.jongewaard.fruit_world.adapters.FruitAdapter;
 import dev.com.jongewaard.fruit_world.models.Fruit;
 
 /**
@@ -23,28 +25,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     //ListView, GridView y Adapters.
     private ListView listView;
     private GridView gridView;
+    private FruitAdapter adapterListView;
+    private FruitAdapter adapterGridView;
 
-    //Lista de nuestro modelo, fruta.
+    // Lista de nuestro modelo, fruta
+    private List<Fruit> fruits;
 
-
+    // Items en el option menu
     private MenuItem itemListView;
     private MenuItem itemGridView;
+
+    // Variables
+    private int counter = 0;
+    private final int SWITCH_TO_LIST_VIEW = 0;
+    private final int SWITCH_TO_GRID_VIEW = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Mostrar icono. (El método esta mas abajo)
         this.enforceIconBar();
 
-        this.listView = (ListView) findViewById(R.id.listView);
-        //this.gridView = (GridView) findViewById(R.id.gridView);
+        this.fruits = getAllFruits();
 
-        //Adjuntando el mismo método click para ambos.
+        //los tomo del activity_main.xml
+        this.listView = (ListView) findViewById(R.id.listView);
+        this.gridView = (GridView) findViewById(R.id.gridView);
+
+        // Adjuntando el mismo método click para ambos
         this.listView.setOnItemClickListener(this);
         this.gridView.setOnItemClickListener(this);
+
+        this.adapterListView = new FruitAdapter(this, R.layout.list_view_item_fruit, fruits);
+        this.adapterGridView = new FruitAdapter(this, R.layout.grid_view_item_fruit, fruits);
+
+        this.listView.setAdapter(adapterListView);
+        this.gridView.setAdapter(adapterGridView);
+
+        // Registrar el context menu para ambos
+        registerForContextMenu(this.listView);
+        registerForContextMenu(this.gridView);
     }
 
     //Método para mostrar el Icono en la barra de menú.
@@ -56,7 +78,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        this.clickFruit(fruits.get(position));
 
+    }
+
+    private void clickFruit(Fruit fruit) {
+        if(fruit.getOrigin().equals("Unknown"))
+            Toast.makeText(this, "Sorry we don't have many info about " + fruit.getName(), Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "The best fruit from " + fruit.getOrigin() + " is " + fruit.getName(), Toast.LENGTH_SHORT).show();
     }
 
     private List<Fruit> getAllFruits(){
